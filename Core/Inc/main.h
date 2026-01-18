7a2178e9590e4a2b0b551bfe40d77dde63e7dddc
“HarryLiugit config --global user.email harryliu1529@163.comgit config --global core.autocrlf falsegit inittouch .gitignoregit config --global user.name “HarryLiu <harryliu1529@163.com>
2026-01-18 12:30:16 +0800

    添加激光器和蓝牙引脚定义，更新GPIO初始化，生成HEX和BIN文件

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,12 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define Lazer_Pin GPIO_PIN_0
+#define Lazer_GPIO_Port GPIOA
+#define BT_TX_Pin GPIO_PIN_2
+#define BT_TX_GPIO_Port GPIOA
+#define BT_RX_Pin GPIO_PIN_3
+#define BT_RX_GPIO_Port GPIOA
 #define RBIN2_Pin GPIO_PIN_0
 #define RBIN2_GPIO_Port GPIOB
 #define LAIN1_Pin GPIO_PIN_13
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "stm32f1xx_hal_tim.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -188,9 +187,21 @@ int main(void)
   
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
   HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+  
+  for (uint8_t i = 0; i < 10; i++)
+  {
+    HAL_GPIO_WritePin(Lazer_GPIO_Port, Lazer_Pin, GPIO_PIN_SET);
+    HAL_Delay(100);
+    HAL_GPIO_WritePin(Lazer_GPIO_Port, Lazer_Pin, GPIO_PIN_RESET);
+    HAL_Delay(100);
+  }
 
-  SetSpeed_L(70);
-  SetSpeed_R(70);
+
+  SetSpeed_L(25);
+  SetSpeed_R(-25);
+  HAL_Delay(1000);
+  SetSpeed_L(0);
+  SetSpeed_R(0);
 
   HAL_UART_Receive_IT(&huart2, &rx_data, 1);
   /* USER CODE END 2 */
